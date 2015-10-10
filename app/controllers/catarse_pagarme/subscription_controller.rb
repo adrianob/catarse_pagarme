7ca95b736433ad86fd4f8322cd28d459f3d7becf
Adriano Benin <adriano.benin@gmail.com>
2015-10-10 14:49:45 -0300

    add subs support

---
+++ app/controllers/catarse_pagarme/subscription_controller.rb
@@ -0,0 +1,38 @@
+module CatarsePagarme
+  class SubscriptionController < CatarsePagarme::ApplicationController
+    MAX_SOFT_DESCRIPTOR_LENGTH = 13
+
+    def create
+      payment_method = params[:payment_method]
+      plan = PagarMe::Plan.find_by_id(subscription.plan.gateway_id)
+
+      info_hash = {
+        postback_url: ipn_pagarme_index_url(
+          host: CatarsePagarme.configuration.host,
+          subdomain: CatarsePagarme.configuration.subdomain,
+          protocol: CatarsePagarme.configuration.protocol
+        ),
+        :customer => {
+          :email => subscription.user.email
+        }
+      }
+      info_hash[:payment_method] = 'boleto' if payment_method == 'slip'
+      info_hash[:card_hash] = params[:card_hash] if payment_method == 'credit_card'
+
+      pagarme_subscription = PagarMe::Subscription.new(info_hash)
+
+      pagarme_subscription.plan = plan
+
+      pagarme_subscription.create
+
+      subscription.update_attribute :state, pagarme_subscription.status
+      subscription.update_attribute :gateway_data, pagarme_subscription.to_json
+
+      response = { boleto_url: pagarme_subscription.current_transaction.boleto_url, payment_status: pagarme_subscription.status }
+      render json: response
+    rescue PagarMe::PagarMeError => e
+      render json: { payment_status: 'failed', message: e.message }
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ CatarsePagarme::Engine.routes.draw do
 
     member do
       get  :review
+      get  :subscription_review
       get  :second_slip, to: 'slip#update'
       post :pay_credit_card, to: 'credit_cards#create'
       post :pay_slip, to: 'slip#create'
+      post :pay_subscription, to: 'subscription#create'
     end
 
     collection do
